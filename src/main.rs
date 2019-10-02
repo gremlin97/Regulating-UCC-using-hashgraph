8c9d927d66ed41e965c95d36c2da2660670acc68
Alakazam03 <vaibhav.045@iiitb.org>
2019-10-02 17:05:54 +0530

    Added Prometheus for metric collection

--- src/main.rs
+++ src/main.rs
@@ -17,11 +17,14 @@ extern crate recrypt;
 extern crate merkle_tree;
 extern crate crypto;
 extern crate bytes;
+use rocket_prometheus::PrometheusMetrics;
 
-
-use merkle_tree::{MerkleTree, SerializationFormat};
 use crypto :: digest :: Digest;
 use crypto :: sha2 :: Sha256;
+use merkletree_rs::{db, MerkleTree, TestValue};
+use std::sync::Mutex;
+
+
 
 mod smt_number;
 mod vid_generation;
@@ -31,16 +34,9 @@ mod messagestatus;
 
 
 
-
-// lazy_static! {
-//     static ref sto : Arc<Mutex<db::Db>> = db::Db::new("test".to_string(), true);
-// //    pub static ref mt : MerkleTree<'static> = MerkleTree::new(140 as u32);
-// //    static ref mt: Mutex<MerkleTree<'static>> = {
-// //        let mut m = MerkleTree::new(&mut sto, 140 as u32);
-// //        Mutex::new(m)
-// //    };
-
-// }
+struct HitCount {
+    sto : Mutex<db::Db>,
+}
 
 
 pub  fn  hash_leaf (value: & [ u8 ]) -> [ u8 ; 32 ] {
@@ -54,38 +50,29 @@ pub  fn  hash_leaf (value: & [ u8 ]) -> [ u8 ; 32 ] {
 
 fn main() {
     println!("Starting filtering service..");
-    let spar = "b".to_string().into_bytes();
-    let serialized_hashed_a =
-            hash_leaf ( &spar);
-    
-    // println!("{:?}", &spar);
-     let mut merkle_tree = MerkleTree::from(&mut ["a", "b", "c"], SerializationFormat::Json);
-    // вызываем построение дерева    
-    merkle_tree.build().unwrap();
-    // печатаем вычисленный хэш рут дерева
-    println!("Merkle tree root hash: {:?}", merkle_tree.get_merkle_root());
-    // печатаем пруф-путь для транзакции b
-    println!("Merkle tree audit proot: {:?}", merkle_tree.audit_proof(&[172, 141, 131, 66, 187, 178, 54,
-     45, 19, 240, 165, 89, 163, 98, 27, 180, 7, 1, 19, 104, 137, 81, 100, 182, 40, 165, 79, 127, 195, 63, 
-     196, 60])
-     .unwrap());
-    // добавляем в дерево хэш транзакции d
-    merkle_tree.push(&String::from("d"));
-    // печатаем рут хэш дерева
-    println!("Merkle tree root hash: {:?}", merkle_tree.get_merkle_root());
-    // печатаем пруф-путь для транзакции b
-    println!("Merkle tree audit proot: {:?}", merkle_tree.audit_proof(&[172, 141, 131, 66, 187, 178, 54, 
-    45, 19, 240, 165, 89, 163, 98, 27, 180, 7, 1, 19, 104, 137, 81, 100, 182, 40, 165, 79, 127, 195, 63, 
-    196, 60])
-    .unwrap());
-    
+    let prometheus = PrometheusMetrics::new();
     rocket::ignite()
-        // .manage(MySto{ sto : db::Db::new("test".to_string(), true)})
-        .mount("/", routes![smt_number::register_customer, smt_number::set_reminders, tsp1::initiate_call, tsp1::terminate_call, smt_number::add_customer, messagestatus::check_user_pref])
+        .attach(prometheus.clone())
+        .mount("/metrics", prometheus)
+        .mount("/", routes![register_customer,  tsp1::initiate_call, tsp1::terminate_call, smt_number::add_customer, messagestatus::check_user_pref])
         .mount("/vid", routes![vid_generation::generate_splits, vid_generation::recover_secret])
         .mount("/add", routes![smt_number::add_customer])
         .launch();
 }
 
+#[get("/<number>/<name>")]
+pub fn register_customer(number : String, name: String) -> String {
 
+    let phone_number : String = number.to_string();
+    let mut sto = db::Db::new("test".to_string(), true);
+    let mut mt = MerkleTree::new(&mut sto, 140 as u32);
+    let mut val: TestValue = TestValue {
+        bytes: phone_number.as_bytes().to_vec(),
+        index_length: 10,
+    };
+    mt.add(&val).unwrap();
+    mt.print_full_tree();
+    format!("Hi")
+
+}
 
